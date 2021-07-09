17aff9759d4bd8dd82027e2244ec3b1659627245
Jason Chan <jason741852@gmail.com>
2021-07-08 19:46:33 -0700

    added listItem and toolbar styles; fixed textfield style

--- src/client/modules/tagListPage/components/ListToolbar.tsx
+++ src/client/modules/tagListPage/components/ListToolbar.tsx
@@ -3,6 +3,8 @@ import { Button } from '../../../shared/components/button';
 import tagListPageContext from '../tagListPageContext';
 import { ListSearch } from './ListSearch';
 
+import './ListToolbar.scss';
+
 export const ListToolbar = () => {
   const { onGenerateRandomTags, onResetTags } = useContext(
     tagListPageContext,
@@ -10,12 +12,20 @@ export const ListToolbar = () => {
 
   return (
     <div className={'list-toolbar'}>
-      <Button
-        label="Populate random tags"
-        onClick={onGenerateRandomTags}
-      />
-      <Button label="Reset tags" onClick={onResetTags} />
-      <ListSearch />
+      <div className={'list-toolbar__button-group'}>
+        <div className={'list-toolbar__button'}>
+          <Button
+            label="Populate random tags"
+            onClick={onGenerateRandomTags}
+          />
+        </div>
+        <div className={'list-toolbar__button'}>
+          <Button label="Reset tags" onClick={onResetTags} />
+        </div>
+      </div>
+      <div className={'list-toolbar__search-field'}>
+        <ListSearch />
+      </div>
     </div>
   );
 };
